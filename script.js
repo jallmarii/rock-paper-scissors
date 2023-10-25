025ace919d87097b67b60344b2494400273929c4
Jallmarii <rickmichael@live.com>
2023-10-25 16:12:09 -0500

    Move variables, close early if null playerChoice

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ function capitalize(input) {
   return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
 };
 
-// I have to define the variables first, it could be below the function due to HOISTING
+// Define the variables first and spend 2 hours learning what hoisting does
+// It's good to define all related variables first then the functions that deal with them after
 const computerChoice = getComputerChoice();
+const playerChoice = getPlayerChoice() 
 
 // Roughly 1/3 chance for any option and return it to store in computerChoice
 function getComputerChoice() {
@@ -27,34 +29,35 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-// Define playerChoice
-const playerChoice = getPlayerChoice() 
-
 function getPlayerChoice() {
-    // valid choices only!
+    // this is an array!
   const validChoices = ['Rock', 'Paper', 'Scissors'];
   let playerChoice = capitalize(prompt('Rock, Paper or Scissors?').trim());
-    // get prompt, then trim any whitespace & convert it to all lowercase for processing
+    // get prompt, trim whitespace, then apply custom function capitalize() to whats left
+    // .trim().capitalize()) isnt allowed because its explicit chaining & javascript not like that 
+    // so, prompt, then trim is applied, then that result is encapsulated by capitalize() so it works
+    // chaining this way is slightly cooler and significantly less readable than just doing them line by line
 
     // chatgpt says to include null, empty or undefined as edge case protection
-    // i started off with just "if validchoices dont include playerchoice" and was very proud of myself
+    // i started off with just "if validchoices dont include playerchoice" and was pretty excited
   if ( 
     playerChoice === null || 
     playerChoice === '' ||    
     playerChoice === undefined ||
     (!validChoices.includes(playerChoice))  
   ) {
-    alert("Didn't work, reload and try again!");
-    return null; // return null just so mr.computer knows / maintain the flow of the program
+    alert("Didn't work, refresh and type in just the word!");
+    return null; // return null so mr.computer knows / maintain the flow of the program
+                 // if i dont have this, it would return undefined, which is not as obvious
   }
    
   return playerChoice; // return the choice to its variable
 }
 
 // This is more readable than a switch statement because there are multiple conditions & multiple variables
-// There are two parameters that go into the function
-
+// There are two parameters for the function
 function whoWins(playerChoice, computerChoice) {
+
   // easy part first
   if (playerChoice === computerChoice) 
   { 
@@ -62,8 +65,6 @@ function whoWins(playerChoice, computerChoice) {
   } 
     
     // thanks to capitalize() it's just a few things instead of a spiderweb
-    // to me this was the hardest part. 
-    // maybe i could do it linking stuff like ${playerChoice} but smarter
     else if (
     (playerChoice === 'Rock') && (computerChoice === 'Scissors') ||
     (playerChoice === 'Paper') && (computerChoice === 'Rock') ||
@@ -77,9 +78,21 @@ function whoWins(playerChoice, computerChoice) {
   }
 }
 
+/* 
+If playerChoice was caught by the error checking on line 43, don't continue the game
+
+I did it with
+
+if (playerChoice) {
+  whoWins(playerChoice, computerChoice);
+}
+
+Then learned there's an epic shorthand way to do it
+*/
+
+// If left operator is truthy, evaluate right operator
+playerChoice && whoWins(playerChoice, computerChoice);
+
 // We define both variables/parameters: playerChoice, computerChoice
 // We determine their value/arguments: Rock, Paper or Scissors
-// We create the logic of how they compare using whoWins()
 // We call whoWins() to explain the winner 
-
-whoWins(playerChoice, computerChoice);
