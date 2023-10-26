28ff624ae26bba187e8a2f34b7f1c8af73d7727c
Jallmarii <rickmichael@live.com>
2023-10-26 11:36:42 -0500

    Add loop and finishing touches

--- script.js
+++ script.js
@@ -4,18 +4,13 @@
 // Compare them
 // Display the results
 
-
-// Take input and standardize it for further use and to display later
+// Utility function to standardize input
 function capitalize(input) {
   return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
 };
 
-// Define the variables first and spend 2 hours learning what hoisting does
-// It's good to define all related variables first then the functions that deal with them after
-const computerChoice = getComputerChoice();
-const playerChoice = getPlayerChoice() 
-
 // Roughly 1/3 chance for any option and return it to store in computerChoice
+// Chatgpt showed a better way to do this but I don't understand it yet
 function getComputerChoice() {
   let num = Math.random() * 3
   let computerChoice;
@@ -30,7 +25,6 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    // this is an array!
   const validChoices = ['Rock', 'Paper', 'Scissors'];
   let playerChoice = capitalize(prompt('Rock, Paper or Scissors?').trim());
     // get prompt, trim whitespace, then apply custom function capitalize() to whats left
@@ -39,8 +33,7 @@ function getPlayerChoice() {
     // chaining this way is slightly cooler and significantly less readable than just doing them line by line
 
     // chatgpt says to include null, empty or undefined as edge case protection
-    // i started off with just "if validchoices dont include playerchoice" and was pretty excited
-  if ( 
+  while ( 
     playerChoice === null || 
     playerChoice === '' ||    
     playerChoice === undefined ||
@@ -48,51 +41,60 @@ function getPlayerChoice() {
   ) {
     alert("Didn't work, refresh and type in just the word!");
     return null; // return null so mr.computer knows / maintain the flow of the program
-                 // if i dont have this, it would return undefined, which is not as obvious
+                 // if i dont have this, it returns undefined, which is not as obvious
   }
-   
-  return playerChoice; // return the choice to its variable
+  return playerChoice; 
 }
 
-// This is more readable than a switch statement because there are multiple conditions & multiple variables
-// There are two parameters for the function
-function whoWins(playerChoice, computerChoice) {
+// Global variables to track score
+// Chatgpt says to make an object but that concept is too complicated for my puny human brain to understand
+let win = 0;
+let tie = 0;
+let lose = 0;
 
+// This is more readable than a switch statement because there are multiple conditions & multiple variables
+function getWinner(computerChoice, playerChoice) {
   // easy part first
-  if (playerChoice === computerChoice) 
+  if (computerChoice === playerChoice) 
   { 
     alert(`It's a tie! You both chose ${computerChoice}.`);
+    return tie += 1; 
   } 
-    
-    // thanks to capitalize() it's just a few things instead of a spiderweb
     else if (
     (playerChoice === 'Rock') && (computerChoice === 'Scissors') ||
     (playerChoice === 'Paper') && (computerChoice === 'Rock') ||
     (playerChoice === 'Scissors') && (computerChoice === 'Paper')
   ) {
     alert(`You won! ${playerChoice} beats ${computerChoice}!`);
+    return win += 1;
 
     // if you dont tie or win, there's only one other option
-  } else {                                   // escaping the frownie face but i dont think it matters
-    alert(`You lost! ${computerChoice} beats ${playerChoice} \:(`) 
+  } else {                                  
+    alert(`You lost! ${computerChoice} beats ${playerChoice} \:(`) //escaping the :( even if it doesn't matter right now)
+    return lose += 1;
   }
 }
 
-/* 
-If playerChoice was caught by the error checking on line 43, don't continue the game
+// Loop for 5 games total - do I capitalize this? I don't camelCase it I think
+const total_games = 5;
+for (let i = 1; i <= total_games; i++) {
 
-I did it with
+  const computerChoice = getComputerChoice();
+  const playerChoice = getPlayerChoice();
 
-if (playerChoice) {
-  whoWins(playerChoice, computerChoice);
+  if (playerChoice) {
+    console.log(`Game ${i}`);
+    getWinner(computerChoice, playerChoice);
+    console.log(`Your current record is ${win} wins, ${lose} losses, and ${tie} ties`);
+  } 
 }
-
-Then learned there's an epic shorthand way to do it
-*/
-
-// If left operator is truthy, evaluate right operator
-playerChoice && whoWins(playerChoice, computerChoice);
 
 // We define both variables/parameters: playerChoice, computerChoice
 // We determine their value/arguments: Rock, Paper or Scissors
 // We call whoWins() to explain the winner 
+// We put this call into a for-loop to play 5 games
+
+
+
+
+
